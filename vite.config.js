b30081c230d1cc276ca61ec725531b26869214a4
Arthur S Dutra <arthurdutra@live.ca>
2024-11-04 01:21:15 -0700

    update for gh page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Chess-game/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
 })
